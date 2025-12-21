bd9ef0df835eb5eb588e5753c199059cae14f32d
leo030195 <hanleolee@gmail.com>
2025-12-21 21:43:34 +0800

    Update Top.v

--- Source/Top.v
+++ Source/Top.v
@@ -13,9 +13,6 @@ module Top (
     output wire [3:0] vgaBlue,
     output wire hsync,
     output wire vsync,
-    // 7 Segment 輸出
-    output wire [6:0] display,
-    output wire [3:0] digit,
     // LED 輸出
     output wire [2:0] led,
     // Debug switch Input
@@ -140,15 +137,7 @@ module Top (
         .my_f_x(P2_f_x),.my_f_y(P2_f_y),.my_r_x(P2_r_x),.my_r_y(P2_r_y),
         .speed_out(p2_speed),.flag(p2_flag_order)
     );
-
-    // 七段顯示器（Debug 用）
-    SevenSegment seven_seg (
-        .display(display),
-        .digit(digit),
-        .nums({p1_speed[7:0], p2_speed[7:0]}),
-        .clk(clk),
-        .rst(rst)
-    );    
+  
 
     // --- 渲染變數 (Rendering Logic) ---
     
@@ -470,75 +459,3 @@ module Top (
 
 endmodule
 
-module SevenSegment(
-    output reg [6:0] display,
-    output reg [3:0] digit,
-    input wire [15:0] nums,
-    input wire rst,
-    input wire clk
-    );
-    
-    reg [15:0] clk_divider;
-    reg [3:0] display_num;
-    
-    always @ (posedge clk, posedge rst) begin
-        if (rst) begin
-            clk_divider <= 15'b0;
-        end else begin
-            clk_divider <= clk_divider + 15'b1;
-        end
-    end
-    
-    always @ (posedge clk_divider[15], posedge rst) begin
-        if (rst) begin
-            display_num <= 4'b0000;
-            digit <= 4'b1111;
-        end else begin
-            case (digit)
-                4'b1110 : begin
-                        display_num <= nums[7:4];
-                        digit <= 4'b1101;
-                    end
-                4'b1101 : begin
-                        display_num <= nums[11:8];
-                        digit <= 4'b1011;
-                    end
-                4'b1011 : begin
-                        display_num <= nums[15:12];
-                        digit <= 4'b0111;
-                    end
-                4'b0111 : begin
-                        display_num <= nums[3:0];
-                        digit <= 4'b1110;
-                    end
-                default : begin
-                        display_num <= nums[3:0];
-                        digit <= 4'b1110;
-                    end             
-            endcase
-        end
-    end
-    
-    always @ (*) begin
-        case (display_num)
-            0  : display = 7'b1000000; // 0000 (0)
-            1  : display = 7'b1111001; // 0001 (1)                                       
-            2  : display = 7'b0100100; // 0010 (2)                                        
-            3  : display = 7'b0110000; // 0011 (3)                                 
-            4  : display = 7'b0011001; // 0100 (4)                                   
-            5  : display = 7'b0010010; // 0101 (5)                                   
-            6  : display = 7'b0000010; // 0110 (6)
-            7  : display = 7'b1111000; // 0111 (7)
-            8  : display = 7'b0000000; // 1000 (8)
-            9  : display = 7'b0010000; // 1001 (9)
-            10 : display = 7'b0001000; // 1010 (A)
-            11 : display = 7'b0000011; // 1011 (b)
-            12 : display = 7'b1000110; // 1100 (C)
-            13 : display = 7'b0100001; // 1101 (d)
-            14 : display = 7'b0000110; // 1110 (E)
-            15 : display = 7'b0001110; // 1111 (F)
-            default : display = 7'b1111111;
-        endcase
-    end
-    
-endmodule
